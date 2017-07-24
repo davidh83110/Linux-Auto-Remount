abfb5e813b681b07d77957437ab38cd17ad985d5
David Hsu <davidh83110@gmail.com>
2017-07-24 14:32:04 +0800

    update sleep time

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ Version 1.0 , 2017-06-30
 #!/usr/bin/env python3
 import os
 import re
+import time
 
 check_shopping = os.popen('/opt/Servers/nagios/libexec/check_file_age -w 650 -c 700 -f /S3Shopping_Content/check_mount').readlines()
 check_shopping = "".join(check_shopping)
@@ -34,7 +35,9 @@ if "status_shopping".join(status_shopping) == status_ok:
     print("ok")
 elif "status_shopping".join(status_shopping) == status_critical:
     umount_shopping = os.popen('umount /S3/Shopping_Content')
+    time.sleep(5)
     mount_shopping = os.popen('mount /S3/Shopping_Content')
+    time.sleep(5)
     mail_shopping = os.popen('echo -e "Auto mount S3_Shopping_Content Sucessfully\n`date`\n`df -kh | grep S3`" | mail -s "`hostname`:Auto mount S3_Shopping_Content Sucessfully" davidh83110@gmail.com')
 else:
     mail_error = os.popen('echo -e "Program ERROR\n`date`\ndf -kh | grep S3" | mail -s "`hostname`:S3 Auto mount Program ERROR" davidh83110@gmail.com')
@@ -45,7 +48,9 @@ if "status_web".join(status_web) == status_ok:
     print("ok")
 elif "status_web".join(status_web) == status_critical:
     umount_shopping = os.popen('umount /S3/Web_Content')
+    time.sleep(5)
     mount_shopping = os.popen('mount /S3/Web_Content')
+    time.sleep(5)
     mail_shopping = os.popen('echo -e "Auto mount S3_Web_Content Sucessfully\n`date`\n`df -kh | grep S3`" | mail -s "`hostname`:Auto mount S3_Web_Content Sucessfully" davidh83110@gmail.com')
 else:
     mail_error = os.popen('echo -e "Program ERROR\n`date`\ndf -kh | grep S3" | mail -s "`hostname`:S3 Auto mount Program ERROR" davidh83110@gmail.com')
